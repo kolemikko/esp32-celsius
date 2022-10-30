88b98a8b00d8336ba198b09a2accc0b18b9e9407
kolemikko <mikko.kolehmainen@tuta.io>
2022-10-30 18:29:16 +0200

    Switched to no-std approach instead of esp-idf

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,41 @@
-use embedded_hal::digital::v2::OutputPin;
-use esp_idf_hal::peripherals::Peripherals;
-use std::thread;
-use std::time::Duration;
+#![no_std]
+#![no_main]
 
-fn main() -> anyhow::Result<()> {
-    esp_idf_sys::link_patches();
+use esp32c3_hal::{
+    clock::ClockControl, gpio::IO, pac::Peripherals, prelude::*, system::SystemExt,
+    timer::TimerGroup, Delay, Rtc,
+};
+use esp_backtrace as _;
+use riscv_rt::entry;
 
+#[entry]
+fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
-    let pins = peripherals.pins;
-    let mut led = pins.gpio19.into_output().unwrap();
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    // Disable the watchdog timers. For the ESP32-C3, this includes the Super WDT,
+    // the RTC WDT, and the TIMG WDTs.
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let mut wdt0 = timer_group0.wdt;
+    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
+    let mut wdt1 = timer_group1.wdt;
+
+    rtc.swd.disable();
+    rtc.rwdt.disable();
+    wdt0.disable();
+    wdt1.disable();
+
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = io.pins.gpio5.into_push_pull_output();
+
+    led.set_high().unwrap();
+
+    let mut delay = Delay::new(&clocks);
 
     loop {
-        led.set_high().unwrap();
-        thread::sleep(Duration::from_secs(2));
-        led.set_low().unwrap();
+        led.toggle().unwrap();
+        delay.delay_ms(1000u32);
     }
-
-    // Ok(())
 }
